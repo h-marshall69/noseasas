5b88fc81b142f7e123648005801ddbce0a7b8810
marshall hunter <marshall.hunter@gmail.com>
2025-10-29 12:00:56 -0500

    se agrego crud

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import TakePhoto from '../views/TakePhoto.vue'
 import FaceDetection from '../views/FaceDetection.vue'
+import Crud from '../views/Crud.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,8 +22,14 @@ const router = createRouter({
       path: '/face-detection',
       name: 'FaceDetection',
       component: FaceDetection
+    },
+    {
+      path: '/crud',
+      name: 'crud-fotos',
+      component: Crud
     }
   ],
 })
 
 export default router
+
---
+++ src/views/Crud.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>relizar crud de las fotos </h1>
+    </div>
+</template>
+
+
+
+<script setup></script>
+
+<style scoped></style>
